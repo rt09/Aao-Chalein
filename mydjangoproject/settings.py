a77135e1ffdff87634ddd9ed6156f0cbc7b039ff
rt09 <78974453+rt09@users.noreply.github.com>
2023-01-05 05:00:00 +0530

    ch

--- mydjangoproject/settings.py
+++ mydjangoproject/settings.py
@@ -173,10 +173,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_BACKEND = 'django_smtp_ssl.SSLEmailBackend'
-EMAIL_HOST_USER = 'rtritik09@gmail.com'
+EMAIL_HOST_USER = 'achalein@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = "yzywxxkfslnlextl"
+EMAIL_HOST_PASSWORD = "jygsbrfwicksismy"
 
 LOGIN_URL = '/Login'
