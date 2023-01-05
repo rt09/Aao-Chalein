63bc0b913b895b02b5c56b82c4a58a919aa81369
rt09 <78974453+rt09@users.noreply.github.com>
2023-01-05 10:23:05 +0530

    ch

--- mydjangoproject/settings.py
+++ mydjangoproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-htf4y#!7-!xg5bmd=yq&wtjj5xfj32zq+fspqk)xldz!+k$gkr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-3a3a.up.railway.app', 'aao-chalein-production.up.railway.app'
+ALLOWED_HOSTS = ['web-production-3a3a.up.railway.app', 'aao-chalein-production.up.railway.app',
                  '127.0.0.1', 'localhost', '0.0.0.0']
 CSRF_TRUSTED_ORIGINS = [
     'https://web-production-3a3a.up.railway.app', 'https://aao-chalein-production.up.railway.app'
